137aa6437c9eea9e8214390eaf837a689daf4b3c
HENNESSYxie <hennessyxie@gmail.com>
2023-03-29 23:38:32 +0500

    add-new-models: carRegister, Employee

--- NPR_web/carRegister/admin.py
+++ NPR_web/carRegister/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Point
+from .models import Point, CarRegister, Employee
 
 # Register your models here.
 admin.site.register(Point)
+admin.site.register(Employee)
+admin.site.register(CarRegister)
--- NPR_web/carRegister/models.py
+++ NPR_web/carRegister/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -9,4 +10,34 @@ class Point(models.Model):
     date = models.DateTimeField()
 
     def __str__(self):
-        return f"({self.x}, {self.y}, {self.camera})"
+        return f"{self.x}, {self.y}, {self.camera}"
+
+
+class Employee(models.Model):
+    fullName = models.CharField('ФИО', max_length=150)
+    car_number = models.CharField('Номер автомобиля', max_length=11)
+    job = models.CharField("Должность", max_length=50)
+    date_of_birth = models.DateField("Дата рождения")
+    car_mark = models.CharField("Марка автомобиля", max_length=50)
+    car_model = models.CharField("Модель автомобиля", max_length=50)
+    car_year = models.IntegerField("Год выпуска автомобиля",
+                                   validators=[
+                                       MaxValueValidator(2023),
+                                       MinValueValidator(1930)
+                                   ]
+                                   )
+
+    def __str__(self):
+        return f"{self.fullName}, {self.job}"
+
+
+class CarRegister(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    type_of_action = models.CharField("Название действия", max_length=20)
+    date = models.DateTimeField("Время")
+    image = models.ImageField(upload_to='carRegister/')
+
+    def __str__(self):
+        return f"{self.employee.fullName}, {self.date}"
+
+
